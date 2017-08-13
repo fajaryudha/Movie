a684896465ca94a12d11dddc62ecf055cd2702d0
Fajar Yudha Pratama <fajaryudha23@gmail.com>
2017-08-13 16:52:37 +0700

    Create Directory

--- app/src/main/java/com/blogspot/fajaryudha23/newmovie/MainActivity.java
+++ app/src/main/java/com/blogspot/fajaryudha23/newmovie/MainActivity.java
@@ -1,13 +1,18 @@
 package com.blogspot.fajaryudha23.newmovie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.blogspot.fajaryudha23.newmovie.NowPlaying.NowPlaying;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        CardView cv_now_playing = (CardView) findViewById(R.id.cardView_now_playing);
+        CardView cv_popular = (CardView) findViewById(R.id.cardView_popular);
+        CardView cv_top_rated = (CardView) findViewById(R.id.cardView_top_rated);
+        CardView cv_upcoming = (CardView) findViewById(R.id.action_upcoming);
+        RecyclerView rv_now_playing = (RecyclerView) findViewById(R.id.recyclerview_now_playing);
+        RecyclerView rv_popular = (RecyclerView) findViewById(R.id.cardView_popular);
+        RecyclerView rv_top_rated = (RecyclerView) findViewById(R.id.cardView_top_rated);
+        RecyclerView rv_upcoming = (RecyclerView) findViewById(R.id.action_upcoming);
+
+        cv_now_playing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NowPlaying.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/blogspot/fajaryudha23/newmovie/NowPlaying/NowPlaying.java
@@ -0,0 +1,15 @@
+package com.blogspot.fajaryudha23.newmovie.NowPlaying;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.blogspot.fajaryudha23.newmovie.R;
+
+public class NowPlaying extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now_playing);
+    }
+}
